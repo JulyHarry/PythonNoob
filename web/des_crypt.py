ab6b913f4cd5f6cb4cc39ce91ce55e54f4d43d73
Harry <xuexunhao@gmail.com>
2022-06-28 23:21:30 +0800

    完善加密功能

--- web/des_crypt.py
+++ web/des_crypt.py
@@ -7,18 +7,11 @@ def des_encrypt(secret, s):
     iv = secret
     k = des(secret, CBC, iv, pad=None, padmode=PAD_PKCS5)
     en = k.encrypt(s, padmode=PAD_PKCS5)
-    return binascii.b2a_hex(en)
+    return binascii.b2a_hex(en).decode('utf-8')
 
 
 def des_decrypt(secret, s):
     iv = secret
     k = des(secret, CBC, iv, pad=None, padmode=PAD_PKCS5)
     de = k.decrypt(binascii.a2b_hex(s), padmode=PAD_PKCS5)
-    return de
-
-
-key = "12345678"
-a = des_encrypt(key, "foobar")
-b = des_decrypt(key, a)
-print(a)
-print(b)
+    return de.decode('utf-8')
